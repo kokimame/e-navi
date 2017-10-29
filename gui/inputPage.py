66dfde1418fee40f1e3daa1fae3bbba551b77e9a
kokimame <kokimametani@gmail.com>
2017-10-29 20:35:50 +0900

    Added question samples to the Q&A table

--- gui/inputPage.py
+++ gui/inputPage.py
@@ -11,7 +11,17 @@ class InputPage(QWidget):
     def initUi(self):
         self.form = forms.enaviInput.Ui_Form()
         self.form.setupUi(self)
+        self.setupTable()
         self.form.endBtn.clicked.connect(self.goNextPage)
+
+    def setupTable(self):
+        table = self.form.qaTable
+        table.setRowCount(10)
+        table.setColumnWidth(0, 340)
+        table.setColumnWidth(1, 340)
+        [table.setItem(i, 0,
+                QTableWidgetItem("これは " + str(i+1) + "番目の質問です")) for i in range(10)]
+
 
     def goNextPage(self):
         self.mw.pageStack.append(2)
