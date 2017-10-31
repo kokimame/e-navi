e40f04d89d53f606beef85333994acf9cf725e0a
kokimame <kokimametani@gmail.com>
2017-10-31 23:37:28 +0900

    Setup FirmTable

--- gui/adminFirm.py
+++ gui/adminFirm.py
@@ -16,6 +16,17 @@ class AdminFirm(QWidget):
                                         msg="変更を適応しますか？"))
         self.form.endBtn.clicked.connect(lambda: self.mw.dm.open("PopupDialog", self.mw,
                                         msg="終了しますか？"))
+        self.setupTable()
+
+    def setupTable(self):
+        table = self.form.firmTable
+        table.setColumnCount(4)
+        table.setColumnWidth(0, 30)
+        table.setColumnWidth(1, 300)
+        table.setColumnWidth(2, 220)
+        table.setColumnWidth(3, 140)
+        table.verticalHeader().setVisible(False)
+        table.setRowCount(100)
 
     def goPage(self, pageName):
         self.mw.pm.setPage(pageName)
