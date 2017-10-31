764752bd630c0886a95d543cbe21e709a415cc6e
kokimame <kokimametani@gmail.com>
2017-10-31 23:54:56 +0900

    Remove popup when main pages change

--- gui/popupDialog.py
+++ gui/popupDialog.py
@@ -5,16 +5,20 @@ class PopupDialog(QDialog):
     def __init__(self, mw, *args, **kwargs):
         QDialog.__init__(self, mw, Qt.Window)
         self.mw = mw
+        self.kwargs = kwargs
         self.form = forms.popupDialog.Ui_PopupDialog()
         self.form.setupUi(self)
         self.form.cancelBtn.clicked.connect(self.reject)
         if "msg" in kwargs:
             self.form.msgLabel.setText(kwargs["msg"])
-
-        if "okTrigger" in kwargs:
-            self.form.okBtn.clicked.connect(kwargs["okTrigger"])
+        self.form.okBtn.clicked.connect(self.okTrigger)
 
         self.show()
+
+    def okTrigger(self):
+        if "okTrigger" in self.kwargs:
+            self.kwargs["okTrigger"]()
+        self.reject()
 
     def reject(self):
         self.done(0)
