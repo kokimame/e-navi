bb339d514e80da073d144de1e4345a65fa628b6d
kokimame <kokimametani@gmail.com>
2017-11-19 13:25:31 +0900

    Added buttons to delete items in a combobox

--- gui/adminEntry.py
+++ gui/adminEntry.py
@@ -6,17 +6,37 @@ class AdminEntry(QWidget):
     def __init__(self, mw):
         super().__init__()
         self.mw = mw
-        self.initUi()
-
-    def initUi(self):
         self.form = forms.adminEntry.Ui_Form()
         self.form.setupUi(self)
-        self.form.dbBtn.clicked.connect(lambda: self.goPage("AdminFirm"))
-        self.form.qaBtn.clicked.connect(lambda: self.goPage("AdminQA"))
-        self.form.applyBtn.clicked.connect(lambda: self.mw.dm.open("PopupDialog", self.mw,
+        self.setupButtons()
+
+    def setupButtons(self):
+        form = self.form
+        form.dbBtn.clicked.connect(lambda: self.goPage("AdminFirm"))
+        form.qaBtn.clicked.connect(lambda: self.goPage("AdminQA"))
+        form.applyBtn.clicked.connect(lambda: self.mw.dm.open("PopupDialog", self.mw,
                                         msg="変更を適応しますか？", okTrigger=lambda: self.goPage("AdminFirm")))
-        self.form.endBtn.clicked.connect(lambda: self.mw.dm.open("PopupDialog", self.mw,
+        form.endBtn.clicked.connect(lambda: self.mw.dm.open("PopupDialog", self.mw,
                                         msg="終了しますか？", okTrigger=lambda: self.goPage("HomePage")))
+        form.fieldDelBtn.clicked.connect(lambda: self.deleteListWidgetItem(form.fieldList))
+        form.jobDelBtn.clicked.connect(lambda: self.deleteListWidgetItem(form.jobList))
+        form.genDelBtn.clicked.connect(lambda: self.deleteListWidgetItem(form.generalList))
+        form.teachDelBtn.clicked.connect(lambda: self.deleteListWidgetItem(form.teachList))
+
+    def deleteListWidgetItem(self, list):
+        if len(list.selectedItems()) == 0:
+            return
+
+        for item in list.selectedItems():
+            for i in range(list.count()):
+                if list.item(i) == item:
+                    list.takeItem(i)
+                    break
+
+        list.repaint()
+
+
+
 
 
     def goPage(self, pageName):
