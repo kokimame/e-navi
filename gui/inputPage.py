5ce58e69d228615c6710c1164e78346b06fd60e7
kokimame <kokimametani@gmail.com>
2017-11-24 21:54:51 +0900

    Added answer box and new warning label

--- gui/inputPage.py
+++ gui/inputPage.py
@@ -1,6 +1,17 @@
 import forms
 from qt import *
 
+class AnswerBox(QWidget):
+    def __init__(self):
+        super(AnswerBox, self).__init__()
+        hbox = QHBoxLayout()
+        hbox.addWidget(QRadioButton("4"))
+        hbox.addWidget(QRadioButton("3"))
+        hbox.addWidget(QRadioButton("2"))
+        hbox.addWidget(QRadioButton("1"))
+        self.setLayout(hbox)
+
+
 
 class InputPage(QWidget):
     def __init__(self, mw):
@@ -11,6 +22,7 @@ class InputPage(QWidget):
     def initUi(self):
         self.form = forms.enaviInput.Ui_Form()
         self.form.setupUi(self)
+        self.form.unfillLbl.setVisible(False)
         self.setupTable()
         self.form.nextBtn.clicked.connect(lambda: print("Move to the next QA page"))
         self.form.backBtn.clicked.connect(lambda: self.goPage("EntryPage"))
@@ -18,11 +30,18 @@ class InputPage(QWidget):
 
     def setupTable(self):
         table = self.form.qaTable
+        table.setSelectionMode(QAbstractItemView.NoSelection)
         table.setRowCount(10)
         table.setColumnWidth(0, 340)
         table.setColumnWidth(1, 340)
         [table.setItem(i, 0,
                 QTableWidgetItem("これは " + str(i+1) + "番目の質問です")) for i in range(10)]
 
+        [table.setCellWidget(i, 1, AnswerBox()) for i in range(10)]
+
+
     def goPage(self, pageName):
+        if not self.form.unfillLbl.isVisible():
+            self.form.unfillLbl.setVisible(True)
+            return
         self.mw.pm.setPage(pageName)
