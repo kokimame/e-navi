9c0d56b106b4e9d96f3b9cce1773f0ca5ec32c45
kokimame <kokimametani@gmail.com>
2017-11-25 21:32:38 +0900

    Remove attributes for Entry items

--- gui/adminEntry.py
+++ gui/adminEntry.py
@@ -45,57 +45,33 @@ class AdminEntry(QWidget):
                     "情報通信業", "運輸業・郵便業", "卸売業，小売業", "金融業，保険業", "不動産業，物品賃貸業",
                     "学術研究，専門・技術サービス業", "宿泊業，飲食サービス業", "生活関連サービス業，娯楽業",
                     "教育，学習支援業", "医療，福祉","複合サービス事業", "サービス業", "公務"]
-        form.fieldList.setStyleSheet("""
-                            QListWidget::item { border-bottom: 1px solid black; }
-                            QListWidget::item:selected { background: rgba(0,255,255,30); }
-                           """)
+
         for lbl in fieldLbl:
-            lwi = QListWidgetItem()
-            la = LblAttr(lbl)
-            lwi.setSizeHint(la.sizeHint())
+            lwi = QListWidgetItem(lbl)
             form.fieldList.addItem(lwi)
-            form.fieldList.setItemWidget(lwi, la)
 
         # Job list -------
         jobLbl = ["営業職","企画管理","事務・アシスタント","販売・サービス職","コンサル・専門事務所","金融系専門職",
                   "公務員・教員・農林水産関連職","技術職","医療系専門職","クリエイター・クリエイティブ職"]
-        form.jobList.setStyleSheet("""
-                            QListWidget::item { border-bottom: 1px solid black; }
-                            QListWidget::item:selected { background: rgba(0,255,255,30); }
-                           """)
+
         for lbl in jobLbl:
-            lwi = QListWidgetItem()
-            la = LblAttr(lbl)
-            lwi.setSizeHint(la.sizeHint())
+            lwi = QListWidgetItem(lbl)
             form.jobList.addItem(lwi)
-            form.jobList.setItemWidget(lwi, la)
 
         # General certification list -------
         genLbl = ["基本情報処理技術者","応用情報処理技術者","行政書士","司法書士",
                   "社会保険労務士","宅件建物取引士","簿記3級","簿記2級","介護福祉士","気象予報士"]
-        form.generalList.setStyleSheet("""
-                            QListWidget::item { border-bottom: 1px solid black; }
-                            QListWidget::item:selected { background: rgba(0,255,255,30); }
-                           """)
+
         for lbl in genLbl:
-            lwi = QListWidgetItem()
-            la = LblAttr(lbl)
-            lwi.setSizeHint(la.sizeHint())
+            lwi = QListWidgetItem(lbl)
             form.generalList.addItem(lwi)
-            form.generalList.setItemWidget(lwi, la)
 
         # Teaching certification list -----
         teachLbl = ["教員免許高一種","教員免許中一種","教員免許小一種","教員免許幼一種","養護教諭一種"]
-        form.teachList.setStyleSheet("""
-                            QListWidget::item { border-bottom: 1px solid black; }
-                            QListWidget::item:selected { background: rgba(0,255,255,30); }
-                           """)
+
         for lbl in teachLbl:
-            lwi = QListWidgetItem()
-            la = LblAttr(lbl)
-            lwi.setSizeHint(la.sizeHint())
+            lwi = QListWidgetItem(lbl)
             form.teachList.addItem(lwi)
-            form.teachList.setItemWidget(lwi, la)
 
 
     def deleteListWidgetItem(self, list):
