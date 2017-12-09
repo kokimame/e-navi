252908753be080be7e6c6a7d1767360d0653c600
kokimame <kokimametani@gmail.com>
2017-12-09 14:37:41 +0900

    Update ui

--- gui/adminEntry.py
+++ gui/adminEntry.py
@@ -2,13 +2,21 @@ import forms
 from qt import *
 from adminQA import AttrBox
 
-FIELDLBL = ["農業，林業", "漁業", "鉱業，採石業，砂利採取業", "建設業", "製造業", "電気・ガス・熱供給・水道業",
+FIELDLBL = ["---", "農業，林業", "漁業", "鉱業，採石業", "建設業", "製造業", "電気・ガス・熱供給",
             "情報通信業", "運輸業・郵便業", "卸売業，小売業", "金融業，保険業", "不動産業，物品賃貸業",
-            "学術研究，専門・技術サービス業", "宿泊業，飲食サービス業", "生活関連サービス業，娯楽業",
+            "学術研究，専門業", "宿泊業，飲食サービス業", "生活サービス業，娯楽業",
             "教育，学習支援業", "医療，福祉", "複合サービス事業", "サービス業", "公務"]
 
-JOBLBL = ["営業職","企画管理","事務・アシスタント","販売・サービス職","コンサル・専門事務所","金融系専門職",
-                  "公務員・教員・農林水産関連職","技術職","医療系専門職","クリエイター・クリエイティブ職"]
+JOBLBL = ["---", "営業職","企画管理","アシスタント","販売・サービス職","コンサル・専門事務所","金融系専門職",
+                  "公務員・教員","技術職","医療系専門職","クリエイティブ職"]
+
+# General certification list -------
+GENLBL = ["---", "基本情報情報処理技術者","応用情報処理情報技術者","行政書士","司法書士",
+                  "社会保険労務士","宅件建物取引士","簿記3級","簿記2級","介護福祉士","気象予報士"]
+
+
+# Teaching certification list -----
+TEACHLBL = ["---", "教員免許高一種","教員免許中一種","教員免許小一種","教員免許幼一種","養護教諭一種"]
 
 
 class LblAttr(QWidget):
@@ -61,18 +69,11 @@ class AdminEntry(QWidget):
             lwi = QListWidgetItem(lbl)
             form.jobList.addItem(lwi)
 
-        # General certification list -------
-        genLbl = ["基本情報処理技術者","応用情報処理技術者","行政書士","司法書士",
-                  "社会保険労務士","宅件建物取引士","簿記3級","簿記2級","介護福祉士","気象予報士"]
-
-        for lbl in genLbl:
+        for lbl in GENLBL:
             lwi = QListWidgetItem(lbl)
             form.generalList.addItem(lwi)
 
-        # Teaching certification list -----
-        teachLbl = ["教員免許高一種","教員免許中一種","教員免許小一種","教員免許幼一種","養護教諭一種"]
-
-        for lbl in teachLbl:
+        for lbl in TEACHLBL:
             lwi = QListWidgetItem(lbl)
             form.teachList.addItem(lwi)
 
--- gui/entryPage.py
+++ gui/entryPage.py
@@ -1,30 +1,41 @@
 import forms
 from qt import *
-
+from adminEntry import FIELDLBL, JOBLBL, GENLBL, TEACHLBL
 
 class EntryPage(QWidget):
     def __init__(self, mw):
         super().__init__()
         self.mw = mw
-        self.initUi()
-
-    def initUi(self):
         self.form = forms.enaviEntry.Ui_Form()
         self.form.setupUi(self)
-        self.form.unfillLbl.setVisible(False)
-        self.form.nextBtn.clicked.connect(lambda: self.goPage("InputPage"))
-        self.form.endBtn.clicked.connect(lambda: self.goPage("HomePage"))
-        # self.setupTable()
+        self.setupButton()
+        self.setupCombo()
 
-    def setupTable(self):
-        table = self.form.ebtryTable
-        table.setRowCount(3)
-        table.setColumnCount(2)
-        table.horizontalHeader().setVisible(False)
-        table.setColumnWidth(0, 340)
-        table.setColumnWidth(1, 340)
-        table.setItem(0, 0, QTableWidgetItem("業種"))
-        table.setItem(1, 0, QTableWidgetItem("職種"))
+    def setupButton(self):
+        form = self.form
+        form.unfillLbl.setVisible(False)
+        form.nextBtn.clicked.connect(lambda: self.goPage("InputPage"))
+        form.endBtn.clicked.connect(lambda: self.goPage("HomePage"))
+       
+    def setupCombo(self):
+        form = self.form
+        form.fieldCombo1.addItems(FIELDLBL)
+        form.fieldCombo2.addItems(FIELDLBL)
+        form.fieldCombo3.addItems(FIELDLBL)
+        form.jobCombo1.addItems(JOBLBL)
+        form.jobCombo2.addItems(JOBLBL)
+        form.jobCombo3.addItems(JOBLBL)
+        form.genCombo1.addItems(GENLBL)
+        form.genCombo2.addItems(GENLBL)
+        form.genCombo3.addItems(GENLBL)
+        form.teachCombo1.addItems(TEACHLBL)
+        form.teachCombo2.addItems(TEACHLBL)
+        form.teachCombo3.addItems(TEACHLBL)
+
+        form.toeflCombo.addItem('---')
+        form.toeicCombo.addItem('---')
+        form.toeicCombo.addItems(['%d' % i for i in range(991)])
+        form.toeflCombo.addItems(['%d' % i for i in range(121)])
 
 
     def goPage(self, pageName):
